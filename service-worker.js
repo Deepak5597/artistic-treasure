66e75391afc8d12fad0d8424ff262151d0e61dfb
Deepak Bisht <32607176+Deepak5597@users.noreply.github.com>
2021-05-02 15:41:33 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
+    ({request}) => (request.path === 'image' || request.path === 'document' || request.path === 'stylesheet'),
     new workbox.strategies.CacheFirst()
 );
